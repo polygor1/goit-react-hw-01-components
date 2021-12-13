d29841a04d6e7428d99bf8d96c757a79a7887884
Igor <polygor@gmail.com>
2021-12-13 18:15:12 +0200

    Updates

---
+++ static/js/main.7a588f89.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpgoit-react-hw-01-components"]=this["webpackJsonpgoit-react-hw-01-components"]||[]).push([[0],{10:function(t,e,n){"use strict";n.r(e);var c=n(1),o=n.n(c),r=n(3),s=n.n(r),i=(n(8),n(0));var a=function(){return Object(i.jsx)("div",{className:"App",children:Object(i.jsx)("h1",{children:"TEST 1.2.3.4.5"})})};s.a.render(Object(i.jsx)(o.a.StrictMode,{children:Object(i.jsx)(a,{})}),document.getElementById("root"))},8:function(t,e,n){}},[[10,1,2]]]);
+//# sourceMappingURL=main.7a588f89.chunk.js.map
