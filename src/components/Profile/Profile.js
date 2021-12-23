c0c98e6a12fa0992ae33029be3ef765bd026c041
Igor <polygor@gmail.com>
2021-12-23 20:51:04 +0200

    Profile.propTypes

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -34,9 +34,15 @@ export default function Profile(props) {
 }
 
 Profile.propTypes = {
-  username: PropTypes.string.isRequired,
-  tag: PropTypes.string.isRequired,
-  location: PropTypes.string.isRequired,
-  avatar: PropTypes.string.isRequired,
-  stats: PropTypes.object.isRequired,
+  props: PropTypes.shape({
+    username: PropTypes.string.isRequired,
+    tag: PropTypes.string.isRequired,
+    location: PropTypes.string.isRequired,
+    avatar: PropTypes.string.isRequired,
+    stats: PropTypes.shape({
+      followers: PropTypes.number,
+      views: PropTypes.number,
+      likes: PropTypes.number,
+    }),
+  }),
 };
